a00d573d5edf96754a83ebea3749b184a49b09a2
Muhammad Notfaji <mohammadnoutfaji1234@gmail.com>
2025-04-21 10:50:26 +0200

    Created the detail page of an item.

--- puddle/urls.py
+++ puddle/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,5 +8,6 @@ from core.views import index, contact
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

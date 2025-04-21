52c56fb95c93f1381543001f401a58fb480f9441
Muhammad Notfaji <mohammadnoutfaji1234@gmail.com>
2025-04-21 16:34:24 +0200

    Created the signup form and page

---
+++ core/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+    
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'e.g. first_last',
+        'class': 'w-full py-2 px-3 rounded-xl'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'example@gmail.com',
+        'class': 'w-full py-2 px-3 rounded-xl'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter at least 8 characters',
+        'class': 'w-full py-2 px-3 rounded-xl'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Re-enter your password',
+        'class': 'w-full py-2 px-3 rounded-xl'
+    }))

3e370c0b295495d81414bd12fcaf12f0cf48dd12
Muhammad Notfaji <mohammadnoutfaji1234@gmail.com>
2025-05-03 20:37:12 +0200

    Added the Edit Item fearure

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -26,7 +26,26 @@ def new(request):
         form = NewItemForm()
 
     return render(request, 'item/form.html', {
-        'form': form
+        'form': form,
+        'title': 'Add Item'
+    })
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item.save()
+            
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item'
     })
 
 @login_required
